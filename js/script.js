1e94b64a12f4bc35e8b33f46309d4090d5c66eb9
Jason Gonzales <jason.gonzales23@gmail.com>
2011-10-16 14:41:58 -0400

    finishing validattion

--- js/script.js
+++ js/script.js
@@ -10,8 +10,41 @@ $(document).ready(function(){
         function(){
             $(this).html('Download Our Album &dArr;');
         }
-    )
+    );
     
+    
+    $('form').submit(function(){
+       var $val = parseInt($('.priceInput').val());
+       console.log($val);
+       if( $val === 0 ){
+        //console.log('cheapskate');
+        cheapskateMsg();
+         return false;
+       }
+       
+       else if ($val > 0) {
+        //console.log('go ahead');
+        return true;
+       }
+       
+       else {
+        errorMsg();
+        return false;
+       }
+    });
+    
+    function cheapskateMsg(){
+        var html = '<p>Seriously? Nothing? Zero? Nada?<p>';
+        html += '<p>Ok, here\'s your link to <a class="button" href="album/TheRegionals.zip">Download</a></p>';
+        html += '<p>Or reconsider and pay any old pittance you want. Seriously, would $2 kill you?</p>';
+        $('.userMsg').empty().html(html).hide().slideDown();
+    }
+    
+    function errorMsg(){
+        console.log('fires');
+        var html = '<p>It looks like you entered nonsense, try again please.</p>';
+        $('.userMsg').empty().html(html).hide().slideDown();
+    }
 });
 
 
