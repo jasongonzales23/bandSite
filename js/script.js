cc3073a10780ed7bed6bbf53de87ac972fefb3cb
Jason Gonzales <jason.gonzales23@gmail.com>
2011-10-16 12:51:43 -0400

    initial rollout stuf is done

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
-/* Author: 
+/* Author: Jason Gonzales
 
 */
 
-
+$(document).ready(function(){
+    $('.buyIt').hover(
+        function(){
+            $(this).html('Scroll Down Please &dArr;');
+        },
+        function(){
+            $(this).html('Download Our Album &dArr;');
+        }
+    )
+    
+});
 
 
 
